e5624435cb7510fce35a383e60c5a57695617f86
Irelandoracle1 <nairajnr1@gmail.com>
2024-12-29 03:19:46 +0000

    building homepage withdjango generic views

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,12 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"The title of this post is {self.title} | Written by {self.author}"    
+
 
 class Comment(models.Model):
     Post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
@@ -24,6 +30,13 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} | Written by {self.author}"     
+
 
 
 
