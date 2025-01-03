6e6278d39ad0d9421abc85ced1e7ec40cc72a0a3
Irelandoracle1 <nairajnr1@gmail.com>
2025-01-03 03:41:22 +0000

    added authentication

--- codestar/settings.py
+++ codestar/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-%m7%4wv@gu2muj#x=laxvl0hr5k*9f)as@1hg3wq$&3@eyy$'
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'django-blog-olu-badero-126e326b3262.herokuapp.com']
 
 
